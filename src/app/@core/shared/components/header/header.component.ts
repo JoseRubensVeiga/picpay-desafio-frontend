a2f5fd82eb41920f0b8df932723d6696fd15afcd
Binho <jose.rubens.veiga@gmail.com>
2022-04-05 21:23:06 -0300

    feat: adicionando matMenu no header com funcionalidade de logout

--- src/app/@core/shared/components/header/header.component.ts
+++ src/app/@core/shared/components/header/header.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { AuthService } from "src/app/@core/services/auth";
+import { NotificationService } from "src/app/@core/services/notification";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"],
 })
 export class HeaderComponent implements OnInit {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private notification: NotificationService
+  ) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onLogoutClick(): void {
+    this.authService.logout();
+    this.router.navigate(["autenticacao"]);
+
+    this.notification.success("Você saiu com sucesso!");
   }
-
 }
--- src/app/@core/shared/shared.module.ts
+++ src/app/@core/shared/shared.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+
+import { MatMenuModule } from "@angular/material/menu";
+import { MatRippleModule } from "@angular/material/core";
+import { MatIconModule } from "@angular/material/icon";
+
 import { HeaderComponent } from "./components/header/header.component";
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatMenuModule, MatRippleModule, MatIconModule],
   exports: [HeaderComponent],
 })
 export class SharedModule {}
