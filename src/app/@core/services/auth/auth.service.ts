3c4fa6735e5d7ab5ddc9891293ca5b89b1990fc6
Binho <jose.rubens.veiga@gmail.com>
2022-04-05 21:24:58 -0300

    feat: adicionando o authService com funcionalidade de login e logout

--- src/app/@core/services/auth/auth.service.ts
+++ src/app/@core/services/auth/auth.service.ts
@@ -1,9 +1,68 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+
+import { BehaviorSubject, Observable, of, throwError } from "rxjs";
+import { delay, finalize, map, pluck, switchMap, tap } from "rxjs/operators";
+
+import endpoints from "src/environments/endpoints";
+import { SignInRequest } from "../../models/SignInRequest";
+import { IUser, User } from "../../models/User";
+import { mapToClass } from "../../operators/mapToClass";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class AuthService {
+  isLoggedIn$ = new BehaviorSubject(this.getIsLoggedInFromLocalStorage());
 
-  constructor() { }
+  isAttempting$ = new BehaviorSubject(false);
+
+  constructor(private http: HttpClient) {}
+
+  attempt(signInRequest: SignInRequest): Observable<User> {
+    const url = endpoints.account;
+    const params = {
+      email: signInRequest.email,
+      password: signInRequest.password,
+    };
+
+    this.isAttempting$.next(true);
+
+    return this.http.get<IUser[]>(url, { params }).pipe(
+      pluck(0),
+      switchMap(
+        (result) =>
+          result ? of(result) : throwError("Nenhum usuário encontrado") // simula o Bad Request do servidor caso não encontra o usuário
+      ),
+      mapToClass(User),
+      delay(2000),
+      tap(() => this.setLoggedIn(true)),
+      finalize(() => this.isAttempting$.next(false))
+    );
+  }
+
+  logout(): void {
+    this.setLoggedIn(false);
+  }
+
+  private setLoggedIn(isLoggedIn: boolean): void {
+    this.isLoggedIn$.next(isLoggedIn);
+    localStorage.setItem("PIC_PAY_LOGGED_IN", JSON.stringify(isLoggedIn));
+  }
+
+  private getIsLoggedInFromLocalStorage(): boolean {
+    const isLoggedInString = localStorage.getItem("PIC_PAY_LOGGED_IN");
+
+    if (!isLoggedInString) {
+      return false;
+    }
+
+    try {
+      const isLoggedIn = JSON.parse(isLoggedInString);
+
+      return isLoggedIn;
+    } catch {
+      return false;
+    }
+  }
 }
