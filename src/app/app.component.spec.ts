42f79fef00420f3424f44ee1ced8db592680fb76
Binho <jose.rubens.veiga@gmail.com>
2022-04-08 11:05:50 -0300

    feat: removendo tema duplicado

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,13 @@
 import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { TestBed, waitForAsync } from "@angular/core/testing";
+import { RouterTestingModule } from "@angular/router/testing";
 import { AppComponent } from "./app.component";
 
 describe("AppComponent", () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [AppComponent],
-      imports: [HttpClientTestingModule],
+      imports: [HttpClientTestingModule, RouterTestingModule],
     }).compileComponents();
   }));
 
